83937e475ffed043ee2acfbdad2a034ee2f0bdd7
trieu12a22005 <trieu7a2005@gmail.com>
2026-03-08 08:15:58 +0700

    feat: dashboard role

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import Login from 'pages/login/Login';
 import MainLayout from 'components/layouts/MainLayout';
 import Portfolio from 'pages/portfolio';
+import Doctor from './pages/doctor/Doctor';
+import RoleHome from './pages/RoleHome/RoleHome';
 // import './App.css';
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route element={<MainLayout />}>
           <Route path="/login" element={<Login />} />
           <Route index element={<Portfolio />}></Route>
+          <Route path = "/doctor" element = {<Doctor />} />
+          <Route path = "/role_home" element = {<RoleHome />} />
         </Route>
       </Routes>
     </>
